6b366ff8285bf47c4846b7dae1caf16440b18b8b
sheik-noorahammed <your@nsyed6348@gmail.com>
2024-08-27 20:00:33 +0530

    save

--- Parent.java
+++ Parent.java
@@ -1,4 +1,12 @@
-public class Parent{
-// Prepare a Function Here 
-  
+public class Parent {
+    
+    // Method in the Parent class
+    public void parentMethod() {
+        System.out.println("Method from Parent class");
+    }
+    
+    // Another method in the Parent class
+    public void display() {
+        System.out.println("Display method in Parent class");
+    }
 }
